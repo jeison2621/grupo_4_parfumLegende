465e46922fa798b7bc92f667982749811a4c19bb
wilsonstevensCapy <stevenscard06@gmail.com>
2021-11-04 19:59:02 -0500

    sacando public de src

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, './public')));
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.use('/', require('./routes'));
 
