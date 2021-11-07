176678aec0205c0f5ae7ceb86ce886ee788cfcb2
DiegoMauricioDuque <diegomduquec@gmail.com>
2021-11-07 10:46:51 -0500

    ComModificando app.js para coreegir ruta del template engine

--- src/app.js
+++ src/app.js
@@ -2,16 +2,16 @@ const express = require('express');
 const path = require('path');
 const main_routes = require('./routes/main_router'); 
 
-
 const app = express();
 
 app.set('view engine', 'ejs'); 
-app.set('views', '../src'); 
+
+app.set('views', (path.join(__dirname, './views')));
+
+//app.set('views', './src/views');
+
 app.use(express.static(path.join(__dirname, '../public')));
 
-
-
-//app.use('/', require('./routes'));
 app.use('/', main_routes);
 app.use('/login', main_routes);
 
