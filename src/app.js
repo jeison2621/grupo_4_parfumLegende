c13f6711de187aed53db42856678c793f1593f3a
unknown <andres.moyano@misena.edu.co>
2021-11-07 22:51:49 -0500

    agregando const methodOverride = require('method-Override');

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const main_routes = require('./routes/main_router'); 
+const methodOverride = require('method-Override');
 
 const app = express();
 
@@ -11,10 +12,10 @@ app.set('views', (path.join(__dirname, './views')));
 //app.set('views', './src/views');
 
 app.use(express.static(path.join(__dirname, '../public')));
+app.use(methodOverride("_method"));
 
 app.use('/', main_routes);
 app.use('/login', main_routes);
-
 app.use('/register', main_routes);
 app.use('/productCart', main_routes);
 app.use('/productDetail', main_routes);
