722c8c238776c4084851b88708193b4c1f812788
unknown <andres.moyano@misena.edu.co>
2021-11-09 09:51:20 -0500

    estructurando footer

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const path = require('path');
 const main_routes = require('./routes/main_router'); 
-const methodOverride = require('method-Override');
+const methodOverride = require('method-Override');      //Enviar datos de un formulario mediante put o delete
 const app = express();
 
+//implementacion EJs
 app.set('view engine', 'ejs'); 
 
 app.set('views', (path.join(__dirname, './views')));
@@ -18,7 +19,6 @@ app.use('/login', main_routes);
 app.use('/register', main_routes);
 app.use('/productCart', main_routes);
 app.use('/productDetail', main_routes);
-
 app.use('/newProducts', main_routes);
 
 module.exports = app;
