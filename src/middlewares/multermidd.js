a64acd77315d0ea886f2ffb9cf6745998c254d67
unknown <andres.moyano@misena.edu.co>
2021-12-23 13:22:01 -0500

    agregando multer a formulario de registro /Funcional

--- src/middlewares/multermidd.js
+++ src/middlewares/multermidd.js
@@ -13,3 +13,4 @@ const storage = multer.diskStorage({
   
   const upload = multer({ storage: storage })
 
+module.exports = upload;
--- src/routes/user_routes.js
+++ src/routes/user_routes.js
@@ -2,14 +2,14 @@ const express = require('express');
 const router = express.Router();
 const path  = require('path')
 const user_controller = require('../controllers/user_controller')
-
+const upload = require('../middlewares/multermidd')
 
 
 
 
 
 router.get('/registro',(req,res)=>{res.render('registro')});
-router.post('/registro',user_controller.save)
+router.post('/registro',upload.single('img'),user_controller.save)
 
 
 
